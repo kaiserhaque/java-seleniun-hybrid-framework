d3b5ead5462d5a5d841ed6a9ea90ce787b3ba3a6
kaiserhaque <khaque444@gmail.com>
2025-05-07 23:20:36 -0700

    Valid/Invalid test methods added

--- src/test/java/com/vodafone/test/LoginTest.java
+++ src/test/java/com/vodafone/test/LoginTest.java
@@ -1,12 +1,7 @@
 package com.vodafone.test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.vodafone.base.AutomationWrapper;
@@ -16,9 +11,24 @@ public class LoginTest extends AutomationWrapper{
 	public void validloginTest(){
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		//password
+		driver.findElement(By.name("password")).sendKeys("admin123");
 		//login
+		driver.findElement(By.xpath("//button[normalize-space(@type)='submit']")).click();
 		//assert
-		
+		String msg = driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick Launch')]")).getText();
+		Assert.assertEquals(msg, "Quick Launch");
+	}
+	
+	@Test
+	public void invalidloginTest(){
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		//password
+		driver.findElement(By.name("password")).sendKeys("password123");
+		//login
+		driver.findElement(By.xpath("//button[normalize-space(@type)='submit']")).click();
+		//assert
+		String msg = driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid credentials')]")).getText();
+		Assert.assertEquals(msg, "Invalid credentials");
 	}
 
 }
